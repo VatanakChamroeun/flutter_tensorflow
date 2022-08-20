bc4382fb3949c637b851d02a17183c5af15a3493
VatanakChamroeun <ch.vatanak01@gmail.com>
2022-08-20 15:18:55 +0700

    feat: detect object tensorflow lite

---
+++ lib/object_detection_screen.dart
@@ -0,0 +1,147 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tensorflow/main.dart';
+import 'package:tflite/tflite.dart';
+
+class ObjectDetectionScreen extends StatefulWidget {
+  const ObjectDetectionScreen({Key? key}) : super(key: key);
+
+  @override
+  _ObjectDetectionScreenState createState() => _ObjectDetectionScreenState();
+}
+
+class _ObjectDetectionScreenState extends State<ObjectDetectionScreen> {
+  CameraController? cameraController;
+  CameraImage? cameraImage;
+  List? recognitionsList;
+
+  initCamera() {
+    cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+    cameraController?.initialize().then((value) {
+      setState(() {
+        cameraController?.startImageStream(
+          (image) => {
+            cameraImage = image,
+            runModel(),
+          },
+        );
+      });
+    });
+  }
+
+  runModel() async {
+    recognitionsList = await Tflite.detectObjectOnFrame(
+      bytesList: cameraImage!.planes.map((plane) {
+        return plane.bytes;
+      }).toList(),
+      imageHeight: cameraImage!.height,
+      imageWidth: cameraImage!.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResultsPerClass: 1,
+      threshold: 0.4,
+    );
+
+    setState(() {
+      cameraImage;
+    });
+  }
+
+  Future loadModel() async {
+    Tflite.close();
+    await Tflite.loadModel(
+      model: "assets/ssd_mobilenet.tflite",
+      labels: "assets/ssd_mobilenet.txt",
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    cameraController?.stopImageStream();
+    Tflite.close();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadModel();
+    initCamera();
+  }
+
+  List<Widget> displayBoxesAroundRecognizedObjects(Size screen) {
+    if (recognitionsList == null) return [];
+
+    double factorX = screen.width;
+    double factorY = screen.height;
+
+    Color colorPick = Colors.blue.shade900;
+
+    return recognitionsList!.map((result) {
+      return Positioned(
+        left: result["rect"]["x"] * factorX,
+        top: result["rect"]["y"] * factorY,
+        width: result["rect"]["w"] * factorX,
+        height: result["rect"]["h"] * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            border: Border.all(color: Colors.blue.shade900, width: 2.0),
+          ),
+          child: Text(
+            "${result['detectedClass']} ${(result['confidenceInClass'] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              background: Paint()..color = colorPick,
+              color: Colors.black,
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    List<Widget> list = [];
+
+    list.add(
+      Positioned(
+        top: 0.0,
+        left: 0.0,
+        width: size.width,
+        height: size.height - 100,
+        child: SizedBox(
+          height: size.height - 100,
+          child: (!cameraController!.value.isInitialized)
+              ? Container()
+              : AspectRatio(
+                  aspectRatio: cameraController!.value.aspectRatio,
+                  child: CameraPreview(cameraController!),
+                ),
+        ),
+      ),
+    );
+
+    if (cameraImage != null) {
+      list.addAll(displayBoxesAroundRecognizedObjects(size));
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Object Detection')),
+        backgroundColor: Colors.black,
+        body: Container(
+          margin: const EdgeInsets.only(top: 50),
+          color: Colors.black,
+          child: Stack(
+            children: list,
+          ),
+        ),
+      ),
+    );
+  }
+}
